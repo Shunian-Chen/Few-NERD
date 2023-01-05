6290acd718c461eec15b8d16c3325169ad08d004
Shunian <1418314920@qq.com>
2023-01-05 02:29:58 +0000

    train script

--- train.sh
+++ train.sh
@@ -4,22 +4,76 @@
 python3 train_demo.py  --mode inter \
 --lr 1e-4 --batch_size 8 --trainN 5 --N 5 --K 1 --Q 1 \
 --train_iter 10000 --val_iter 500 --test_iter 5000 --val_step 1000 \
---max_length 64 --model structshot --tau 0.32 --fp >> result.txt
+--max_length 64 --model structshot --tau 0.32 >> result.txt
 
 # 5-way 5~10-shot
 python3 train_demo.py  --mode inter \
 --lr 1e-4 --batch_size 1 --trainN 5 --N 5 --K 5 --Q 5 \
 --train_iter 10000 --val_iter 500 --test_iter 5000 --val_step 1000 \
---max_length 32 --model structshot --tau 0.318 --fp >> result.txt
+--max_length 32 --model structshot --tau 0.318 >> result.txt
 
 # 10-way 1~5-shot
 python3 train_demo.py  --mode inter \
 --lr 1e-4 --batch_size 1 --trainN 10 --N 10 --K 1 --Q 1 \
 --train_iter 10000 --val_iter 500 --test_iter 5000 --val_step 1000 \
---max_length 64 --model structshot --tau 0.32 --fp >> result.txt
+--max_length 64 --model structshot --tau 0.32 >> result.txt
 
 # 10-way 5~10-shot
 python3 train_demo.py  --mode inter \
 --lr 1e-4 --batch_size 1 --trainN 10 --N 10 --K 5 --Q 1 \
 --train_iter 10000 --val_iter 500 --test_iter 5000 --val_step 1000 \
---max_length 32 --model structshot --tau 0.434 --fp >> result.txt
+--max_length 32 --model structshot --tau 0.434 >> result.txt
+
+
+
+# 5-way 1~5-shot proto
+python3 train_demo.py  --mode inter \
+--lr 1e-4 --batch_size 8 --trainN 5 --N 5 --K 1 --Q 1 \
+--train_iter 10000 --val_iter 500 --test_iter 5000 --val_step 1000 \
+--max_length 64 --model proto --tau 0.32 >> result.txt
+
+# 5-way 5~10-shot proto
+python3 train_demo.py  --mode inter \
+--lr 1e-4 --batch_size 1 --trainN 5 --N 5 --K 5 --Q 5 \
+--train_iter 10000 --val_iter 500 --test_iter 5000 --val_step 1000 \
+--max_length 32 --model proto --tau 0.318 >> result.txt
+
+# 10-way 1~5-shot proto
+python3 train_demo.py  --mode inter \
+--lr 1e-4 --batch_size 1 --trainN 10 --N 10 --K 1 --Q 1 \
+--train_iter 10000 --val_iter 500 --test_iter 5000 --val_step 1000 \
+--max_length 64 --model proto --tau 0.32 >> result.txt
+
+# 10-way 5~10-shot proto
+python3 train_demo.py  --mode inter \
+--lr 1e-4 --batch_size 1 --trainN 10 --N 10 --K 5 --Q 1 \
+--train_iter 10000 --val_iter 500 --test_iter 5000 --val_step 1000 \
+--max_length 32 --model proto --tau 0.434 >> result.txt
+
+
+
+
+
+# 5-way 1~5-shot nnshot
+python3 train_demo.py  --mode inter \
+--lr 1e-4 --batch_size 8 --trainN 5 --N 5 --K 1 --Q 1 \
+--train_iter 10000 --val_iter 500 --test_iter 5000 --val_step 1000 \
+--max_length 64 --model nnshot --tau 0.32 >> result.txt
+
+# 5-way 5~10-shot nnshot
+python3 train_demo.py  --mode inter \
+--lr 1e-4 --batch_size 1 --trainN 5 --N 5 --K 5 --Q 5 \
+--train_iter 10000 --val_iter 500 --test_iter 5000 --val_step 1000 \
+--max_length 32 --model nnshot --tau 0.318 >> result.txt
+
+# 10-way 1~5-shot nnshot
+python3 train_demo.py  --mode inter \
+--lr 1e-4 --batch_size 1 --trainN 10 --N 10 --K 1 --Q 1 \
+--train_iter 10000 --val_iter 500 --test_iter 5000 --val_step 1000 \
+--max_length 64 --model nnshot --tau 0.32 >> result.txt
+
+# 10-way 5~10-shot nnshot
+python3 train_demo.py  --mode inter \
+--lr 1e-4 --batch_size 1 --trainN 10 --N 10 --K 5 --Q 1 \
+--train_iter 10000 --val_iter 500 --test_iter 5000 --val_step 1000 \
+--max_length 32 --model nnshot --tau 0.434 >> result.txt
